c2ab9a4f93f5ecea0c3cac160ccdb066d5dc6e08
kjhto46 <tony1996kjh@gmail.com>
2023-04-13 23:19:10 +0900

    여러페이지 만들기 (라우터)

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="navbar">
+          <Link href='/'>홈</Link>
+          <Link href='/list'>List</Link>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+
+
 export default function Home() {
+  let name = 'Kwon 준 hee'
   return (
     <div>
-      
+      <h4 className="title">즈리fresh</h4>
+      <p className="title-sub">by dev { name }</p>
     </div>
   )
 }
