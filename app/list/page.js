071c33823c2ecfde60c0c77771f773709f2f2823
kjhto46 <tony1996kjh@gmail.com>
2023-04-14 23:32:13 +0900

    map()반복문

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,17 @@
 export default function List() {
-
+    let 상품 = ['토마토', '파스타', '코코넛']
     return (
       <div>
         <h4 className="title">상품목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
+        {
+          상품.map((a, i)=> {
+              return (
+                <div className="food" key={i}>
+                  <h4>{상품[i]} $40</h4>
+                </div>
+              )
+          })
+        }
       </div>
     )
 }
