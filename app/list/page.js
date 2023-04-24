e2ffe5272db74f34b194408ac9ed2a374aa28ce6
권준희 <81128476+kjhto46@users.noreply.github.com>
2023-04-24 16:42:28 +0900

    use client에서 useState를 사용하여 javascript onclick활성화
    use client에서 useState를 사용하여 javascript onclick활성화

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,10 @@
+'use client'
+
+import { useState } from "react"
+
 export default function List() {
     let 상품 = ['토마토', '파스타', '코코넛']
-
+    let [수량, 수량번경] = useState(0) // state의 장점 state가 변경되면 state 쓰는 html부분도 자동으로 변경됨
     return (
       <div>
         <h4 className="title">상품목록</h4>
@@ -10,6 +14,8 @@ export default function List() {
                 <div className="food" key={i}>
                   <img src={`/food${i}.png`} alt={상품[i]} className="food-img"/>
                   <h4>{상품[i]} $40</h4>
+                  <span>{수량}</span>
+                  <button onClick={() =>{수량번경(수량+1)}}>+</button>
                 </div>
               )
           })
