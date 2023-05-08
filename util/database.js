a39c1843b08bfb8532cec9f8846338cc97a9adde
권준희 <razri@gwonjunhuiui-iMac.local>
2023-05-08 23:48:43 +0900

    망고DB와 연결에 대해 알아보았다

---
+++ util/database.js
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb'
+const url = 'mongodb+srv://razri46:KwonJunHee46@cluster0.lzzgmd0.mongodb.net/?retryWrites=true&w=majority'
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+export { connectDB }
