3d042be0615b631bc5cc71dfd6ae2f68fc032701
kjhto46 <tony1996kjh@gmail.com>
2023-05-16 07:25:30 +0900

    method get 하는법 알아보기

---
+++ pages/api/test.js
@@ -0,0 +1,11 @@
+import { connectDB } from '@/util/database';
+
+export default async function handler(요청, 응답){
+
+   const db = (await connectDB).db("forum")
+   let result = await db.collection('post').find().toArray()
+
+   if (요청.method == 'POST') {
+      return 응답.status(200).json(result);
+   }
+}
