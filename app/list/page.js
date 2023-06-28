e9e86e08c0783c28ddabf45690597a143adfa646
kjhto46 <tony1996kjh@gmail.com>
2023-06-29 00:01:49 +0900

    오늘 하루 테스트 메모

--- app/list/page.js
+++ app/list/page.js
@@ -5,10 +5,11 @@ import DetailLink from "./DetailLink";
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-
+  console.log(result);
   return (
     <div className="list-bg">
-      {result.map((a, i) => ( //return을 하지 않은 이유 알아보긴
+      {result.map((a, i) => ( 
+        //return을 하지 않은 이유 , 원래 화살표함수에서 () => {}, ()함수가 return() 하나밖에 없기때문에  () => () 해서 return이 생략 되었다.
         <div className="list-item" key={i}>
           <Link prefetch={false} href={"/detail/" + result[i]._id}>
             <h4>{result[i].title}</h4>
