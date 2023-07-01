52eb265881b42575374202f1e3daa9cda1f67de3
kjhto46 <tony1996kjh@gmail.com>
2023-07-02 01:59:11 +0900

    use client를 사용인 페이지와 아닌 페이지 연결

---
+++ app/list/ListItem.js
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+
+// import { useEffect } from "react";
+
+export default function ListItem({ result }) {
+  // useEffect(() => {
+  // // !서버에 부탁해서 DB게시물 부탁해서 가져옴 예를 들어 get요청
+  // // result = DB게시물
+  // // !이런식으로 짤수가 있음 하지만 여기는 'use client'를 사용한 [client component] 따라서 검색노출이 어렵다..
+  // },[]) // !useEffect()는 HTML부터 유저에게 보여주고 그다음에 실행된다. 그런 방식때문에 검색노출이 어려움
+
+  // !!!!!따라서 우리는 props로 result를 부모 component에서 가져온다.
+
+  // props.result로 작성을 했었지만 props를 쉽게 쓰는 법이 있었으니~~
+  //  `ListItem(props) {` 이렇게 작성된것을 `ListItem({result}) {` 이렇게 하여 result라는 함수를 props로 불러온것으로 지정 !!! 자세한 건 destructuring 문법 찾아보기
+  return (
+    <div>
+      {result.map((a, i) => (
+        //return을 하지 않은 이유 , 원래 화살표함수에서 () => {}, ()함수가 return() 하나밖에 없기때문에  () => () 해서 return이 생략 되었다. 쉽게 말해 {} 중괄호와 return을 생략하였다고 보면된다
+        <div className="list-item" key={i}>
+          <Link prefetch={false} href={"/detail/" + result[i]._id}>
+            <h4>{result[i].title}</h4>
+            <p>{result[i].content}</p>
+          </Link>
+          <Link href={"/edit/" + result[i]._id}>수정</Link>
+
+          <span
+            onClick={() => {
+              fetch("/api/test", {method:'DELETE'})
+//  !!!!삭제기능 만들어보기 숙제
+              .then(() => {
+                  console.log(22222)
+               })
+            }}
+          >
+            🗑️
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,6 @@
 import { connectDB } from "@/util/database";
 import Link from "next/link";
+import ListItem from "./ListItem";
 // import DetailLink from "./DetailLink";
 // detailLink에 useRouter에 대한 내용 저장함. useRouter은 Link와 다르게 자바스크립트 코드로 페이지를 이동시켜줄 수 있음.
 
@@ -9,16 +10,7 @@ export default async function List() {
   // console.log(result);
   return (
     <div className="list-bg">
-      {result.map((a, i) => ( 
-        //return을 하지 않은 이유 , 원래 화살표함수에서 () => {}, ()함수가 return() 하나밖에 없기때문에  () => () 해서 return이 생략 되었다. 쉽게 말해 {} 중괄호와 return을 생략하였다고 보면된다
-        <div className="list-item" key={i}>
-          <Link prefetch={false} href={"/detail/" + result[i]._id}>
-            <h4>{result[i].title}</h4>
-            <p>{result[i].content}</p>
-          </Link>
-          <Link href={"/edit/" + result[i]._id}>수정</Link>
-        </div>
-      ))}
+      <ListItem result={result} />
     </div>
   );
   //  Link에서는 자동으로 미리 로드해주는 prefetch기능이 있다. 하지만 링크가 많은 게시판의 경우 모든 링크를 다 읽을게 아닌데 모든걸 미리 로드하는 것은 자원낭비이다. {개발중일때는 확인 불가능 나중에 사이트 발행하면 확인 가능하다고 하네요} 
