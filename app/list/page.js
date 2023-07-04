20b24362a6ceea184cfac1fa85475f6cc3c11cd3
kjhto46 <tony1996kjh@gmail.com>
2023-07-04 22:40:47 +0900

    어제 부족한 설명 추가

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,17 @@ import ListItem from "./ListItem";
 // import DetailLink from "./DetailLink";
 // detailLink에 useRouter에 대한 내용 저장함. useRouter은 Link와 다르게 자바스크립트 코드로 페이지를 이동시켜줄 수 있음.
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic' //dynamic이라는 예약된 변수명이 있음. force-dynamic으로 항상 dynamic으로 보여주게 함 ,, 반대로 force-static이라고 하면 항상 static으로 보여줌 
+
+// !! dynamic렌더링은 서버나 DB에 부담기능이 조금있음 따라서 캐싱 기능을 사용할 수 있음. 캐싱이란 GET요청 결과를 잠깐 다른곳에 저장해두고 그거를 재사용한다는 뜻 
+// await fetch('/URL', {cache : 'force-cache'}) // 이방법으로 get요청 결과를 캐싱가능 
+// await fetch('/URL', {cache : 'no-store'}) // 이러면 /URL 로 접근할때마다 매번 서버로 요청해서 새것을 가지고 옴
+// await fetch('/URL', {next : {revalidate : 60}}) // 이러면 이 '/URL'로 요청할때 캐싱을 해주는데 60초동안 캐싱된 데이터 보여줌. 60초 이후에는 다시 캐싱된 데이터를 보여줌.
+
+// !! 우리는 근데 DB를 사용하고 있는데 DB의 출력 결과는 캐싱이 가능한가? 당연히 가능하다 2가지 방법이 있음.
+// !!! 방법 1 db 관련 작업을 API로 만든후 await fetch('/URL') 할때 /URL로 연결하기
+// !!! revalidate라는 예약변수를 사용하면 페이지 단위 캐싱이 가능하다.
+// !!!! export const revalidate = 20 이렇게 작업하면 20초초동안 캐싱을 해준다.
 
 export default async function List() {
   const db = (await connectDB).db("forum");
